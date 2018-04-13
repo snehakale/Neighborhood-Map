e630fb7b212e5d735a813f36b266252d0cba1fbd
Sneha Kale <kalesneha88@gmail.com>
2018-04-12 20:26:36 -0400

    style: Format code and add comments
    Format code and add comments in the files viz. style.css, index.html, app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Locations data
 var locations = [
     { title: 'Georgia Aquarium',
       position: {lat: 33.763333 ,lng: -84.395268 },
@@ -41,16 +42,19 @@ var locations = [
     }
 ]
 
-// foursquare data
+// Foursquare api data
 forsquareUrl = 'https://api.foursquare.com/v2/venues/';
 clientId ='AU1HKMEIUF0ORWNCGQ0B4BPAG5ZYTGNC3RCC1KRI3Q51ULL4';
 clientSecret ='GUAWICBPSYJK5UAEI1X3BA1W24ZAV2QKIWBVMX0EWNG1GCJT';
 version ='20171013';
 
+// global variables
 var map;
 var markers =[];
 var marker;
 
+
+// Function to initialize a map and markers
 var initMap = function(){
     map = new google.maps.Map(document.getElementById('map'), {
            center: {lat: 33.753746, lng: -84.386330},
@@ -67,7 +71,8 @@ var initMap = function(){
     map.fitBounds(bounds);
 };
 
-// location
+
+// location model
 var Location = function(locationData){
     this.title = ko.observable(locationData.title);
     this.position = ko.observable(locationData.position);
@@ -75,15 +80,18 @@ var Location = function(locationData){
     this.id = ko.observable(locationData.id);
 }
 
+
+// ViewModel
 var ViewModel = function(){
     var self = this;
-    //self.markers = markers;
-    self.locationList = ko.observableArray([]);
 
+    // Creating locationList to hold list of locations
+    self.locationList = ko.observableArray([]);
+    // Pushing each location element into locationList
     locations.forEach(function(locationItem){
         var locationItem = new Location(locationItem);
         self.locationList.push(locationItem);
-
+        // Creating marker for each location and pushing it into markers array
         marker = new google.maps.Marker({
                       title: locationItem.title(),
                       position: locationItem.position(),
@@ -92,9 +100,12 @@ var ViewModel = function(){
         markers.push(marker);
     });
 
-    self.selectedLocation = ko.observable();
+    // Creating infowindow object
     var infowindowForMarker = new google.maps.InfoWindow();
 
+    // Creating selectedLocation observable
+    self.selectedLocation = ko.observable();
+    // Function to open infowindow on a marker of selected list item
     self.openMarkerForLocation = function(locationItem){
         self.selectedLocation(locationItem);
         var markerId = self.selectedLocation().id();
@@ -102,42 +113,36 @@ var ViewModel = function(){
         openInfoWindow(map, selectedMarker, infowindowForMarker);
     };
 
+    // Creating filterText observable
     self.filterText = ko.observable("");
-    self.filteredList = ko.observableArray([]);
-
+    // Function to filter list and markers
     self.showFilteredLocations = function(){
-      var filterText= self.filterText();
-      self.locationList.removeAll();
-      //markers = [];
-      //alert("markers size="+markers.length);
-      locations.forEach(function(locationItem){
-          var locationItem = new Location(locationItem);
-          var locationTitle = locationItem.title();
-          if(locationTitle.includes(filterText)){
-            self.locationList.push(locationItem);
-          }
-          else{
-            showFilteredMarker(locationItem.id());
-          }
-          //alert("markers size then="+markers.length);
-      });
+        var filterText= self.filterText();
+        self.locationList.removeAll();
+        locations.forEach(function(locationItem){
+            var locationItem = new Location(locationItem);
+            var locationTitle = locationItem.title();
+            if(locationTitle.includes(filterText)){
+                self.locationList.push(locationItem);
+            }
+            else{
+                showFilteredMarker(locationItem.id());
+            }
+        });
     };
 
+    // Function to reset list and markers
     self.resetLocations = function(){
         self.filterText("");
         self.locationList.removeAll();
-        //markers = [];
         locations.forEach(function(locationItem){
             var locationItem = new Location(locationItem);
             self.locationList.push(locationItem);
             resetMarker(locationItem.id());
-          });
+        });
     };
 
-    /*for(var a=0; a<locations.length; a++){
-        alert("loc ="+locations[a].venueId);
-    }*/
-
+    // Setting markers data
     for(var i=0; i< markers.length; i++){
         marker = markers[i];
         marker.set("markerId",i);
@@ -145,70 +150,72 @@ var ViewModel = function(){
             openInfoWindow(map, this, infowindowForMarker);
         });
     }
-
 }
 
+
+// Function to open InfoWindow for given map and marker
 var openInfoWindow = function(map, marker, infowindowForMarker){
     changeMarkerAnimation(marker, true);
     var newId = marker.get("markerId");
     var venueId = locations[newId].venueId;
+    // url
     var url = forsquareUrl+venueId+'?client_id='+clientId+'&client_secret='+clientSecret+'&v='+version;
-    //console.log("url="+url);
-
     infowindowForMarker.marker = marker;
     var results ="";
     infowindowForMarker.setContent(results);
-
+    // Timeout function
     var infoTimeOut = setTimeout(function(){
       results="Time Out!!";
       infowindowForMarker.setContent('<b><i>'+results+'</i></b>');
     },8000);
-
+    // Calling ajax to get the response
     $.ajax({
         url : url,
         dataType : "jsonp",
-        success : function(data) {
-         //clearTimeout(wikiTimeOutRequest);
+        success : function(data){
            if(data.response){
-             name = data.response.venue.name;
-             contact = data.response.venue.contact.formattedPhone
-             address = data.response.venue.location.formattedAddress;
-             weburl = data.response.venue.url;
-             var line1="";
-             var line2="";
-             var line3="";
-             var line4="";
+               name = data.response.venue.name;
+               contact = data.response.venue.contact.formattedPhone
+               address = data.response.venue.location.formattedAddress;
+               weburl = data.response.venue.url;
+               var line1="";
+               var line2="";
+               var line3="";
+               var line4="";
 
-             if(name){
-               line1 ="<span><b>Name :</b>"+name+"</span><br>";
+               if(name){
+                 line1 ="<span><b>Name :</b>"+name+"</span><br>";
+               }
+               if(contact){
+                 line2 ="<span><b>Contact :</b>"+contact+"</span><br>";
+               }
+               if(address){
+                 line3 ="<span><b>Address :</b>"+address+"</span><br>";
+               }
+               if(weburl){
+                 line4 ="<span><b>For more info visit at :</b><a href='"+weburl+"' target='_blank'>"+weburl+"</a></span>";
+               }
+               results = line1 + line2 + line3 + line4;
              }
-             if(contact){
-               line2 ="<span><b>Contact :</b>"+contact+"</span><br>";
-             }
-             if(address){
-               line3 ="<span><b>Address :</b>"+address+"</span><br>";
-             }
-             if(weburl){
-               line4 ="<span><b>For more info visit at :</b><a href='"+weburl+"' target='_blank'>"+weburl+"</a></span>";
-             }
-             results = line1 + line2 + line3 + line4;
-           }
            infowindowForMarker.setContent(results);
            clearTimeout(infoTimeOut);
-       },
-       error : function(){
-          results ='Unable to load data!!';
-          infowindowForMarker.setContent('<b><i>'+results+'</i></b>');
-       }
-    });
-
-    infowindowForMarker.open(map, marker);
-    infowindowForMarker.addListener('closeclick',function(){
+        },
+        error : function(){
+            results ='Unable to load data!!';
+            infowindowForMarker.setContent('<b><i>'+results+'</i></b>');
+        }
+     });
+     // Opening info window on given maker and map
+     infowindowForMarker.open(map, marker);
+     // Function to close info window for marker
+     infowindowForMarker.addListener('closeclick',function(){
         infowindowForMarker.setMarker = null;
         changeMarkerAnimation(marker, false);
     });
 };
 
+
+// Function to change marker's animation
 var changeMarkerAnimation = function(selectedMarker, booleanVar){
     for(var i=0; i<markers.length; i++){
         if(markers[i]==selectedMarker){
@@ -225,19 +232,27 @@ var changeMarkerAnimation = function(selectedMarker, booleanVar){
     }
 };
 
+
+// Function to show an alert message if map fails to load
 var mapError = function(){
     alert("Map can not be displayed");
 };
 
+
+// Function to hide marker on a map
 var showFilteredMarker = function(id){
     markers[id].setMap(null);
 };
 
+
+// Function to show marker on a map
 var resetMarker = function(id){
     markers[id].setMap(map);
 };
 
+
+// Function to start loading the data
 var start = function(){
-  ko.applyBindings(new ViewModel());
-  initMap();
+    ko.applyBindings(new ViewModel());
+    initMap();
 };
